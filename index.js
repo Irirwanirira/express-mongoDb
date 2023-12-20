9cdc034d80da711f7982e3249c3b2985eee24479
Irirwanirira <irijoseph41@gmail.com>
2023-12-20 23:38:27 +0200

    Add api documentation

--- index.js
+++ index.js
@@ -3,32 +3,23 @@ const swaggerJsdoc = require("swagger-jsdoc")
 const swaggerUi = require("swagger-ui-express")
 const mongoose = require("mongoose")
 const createServer = require("./server")
-
-// const routes = require("./routes/routes")
     
 mongoose.connect("mongodb://localhost:27017/blog-app")
     .then(() => {
-        // const app = express()
         const app = createServer()
-        // app.use(express.json())
-        // app.use("/api", routes)
+
         const options = {
             definition: {
               openapi: "3.1.0",
               info: {
-                title: "LogRocket Express API with Swagger",
+                title: " Express API with Swagger",
                 version: "0.1.0",
                 description:
                   "This is a simple CRUD API application made with Express and documented with Swagger",
                 license: {
                   name: "MIT",
                   url: "https://spdx.org/licenses/MIT.html",
-                },
-                contact: {
-                  name: "LogRocket",
-                  url: "https://logrocket.com",
-                  email: "info@email.com",
-                },
+                }
               },
               servers: [
                 {
@@ -43,7 +34,11 @@ mongoose.connect("mongodb://localhost:27017/blog-app")
           app.use(
             "/api-docs",
             swaggerUi.serve,
-            swaggerUi.setup(specs),
+            swaggerUi.setup(specs, {
+              explorer: true,
+              customCssUrl:
+                "https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-newspaper.css",
+            }),
             swaggerUi.setup(specs, { explorer: true })
           );
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,69 +1,90 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     post:
+ *       type: object
+ *       required:
+ *         - title
+ *         - content
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the Post
+ *         title:
+ *           type: string
+ *           description: The title of your Post
+ *         content:
+ *           type: string
+ *           description: The content author
+ *       example:
+ *         id: d5fE_asz
+ *         title: The New Turing Omnibus
+ *         content: post 1
+ *
+ */
 
-const express = require("express")
-const Post = require("../models/Post")
 
-const router = express.Router()
 
-router.get("/posts", async(req, res) => {
-    const posts = await Post.find()
-    res.send(posts)
-})
+const express = require('express');
+const Post = require('../models/Post');
 
-router.post("/Posts", async(req, res) => {
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content,
+const router = express.Router();
 
-    })
-    await
-     post.save()
-     res.send(post)
-})
+router.get('/posts', async (req, res) => {
+  const posts = await Post.find();
+  res.send(posts);
+});
 
-router.get("/posts/:id", async(req, res) => {
-    try {
-        const id =req.params._id
-        const post = await Post.findOne(id)
-        res.send(post)
-    } catch{
-        res.status(404)
-        res.send({error: "Post doesn't exist!"})
+router.post('/Posts', async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  await post.save();
+  res.send(post);
+});
+
+router.get('/posts/:id', async (req, res) => {
+  try {
+    const id = req.params._id;
+    const post = await Post.findOne(id);
+    res.send(post);
+  } catch {
+    res.status(404);
+    res.send({ error: "Post doesn't exist!" });
+  }
+});
+
+router.patch('/posts/:id', async (req, res) => {
+  try {
+    const id = req.params._id;
+    const post = await Post.findOne(id);
+
+    if (req.body.title) {
+      post.title = req.body.title;
     }
-})
-
-router.patch("/posts/:id", async(req, res) => {
-    try {
-        const id =req.params._id
-        const post = await Post.findOne(id)
-
-        if(req.body.title){
-            post.title = req.body.title
-        }
-        if(req.body.content){
-            post.content = req.body.content
-        }
-
-        await post.save()
-        res.send(post)
-        
-    } catch {
-        res.status(404);
-        res.send({error: "Post doesn't exist"})
+    if (req.body.content) {
+      post.content = req.body.content;
     }
-})
 
-router.delete("/posts/:id", async(req, res) => {
-    try {
-        const id =req.params._id
-        const post = await Post.deleteOne(id)
-        res.status(204).send()
-        
-    } catch {
-        res.status(404)
-        res.send({error: "Post does'nt exist!"})
-        
-    }
-})
-  
+    await post.save();
+    res.send(post);
+  } catch {
+    res.status(404);
+    res.send({ error: "Post doesn't exist" });
+  }
+});
 
-module.exports = router
+router.delete('/posts/:id', async (req, res) => {
+  try {
+    const id = req.params._id;
+    const post = await Post.deleteOne(id);
+    res.status(204).send();
+  } catch {
+    res.status(404);
+    res.send({ error: "Post does'nt exist!" });
+  }
+});
+
+module.exports = router;
